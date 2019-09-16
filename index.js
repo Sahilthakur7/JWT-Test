50cc2866be087c7b47905c74c0ddeb65a41e2557
sahilthakur7 <sahilrd07@gmail.com>
2019-09-16 21:19:02 +0530

    user model created

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
 const authRoute = require('./routes/auth');
+const keys = require('./config/keys');
+const dotenv = require('dotenv');
 
-app.listen(3000 , () => console.log("server started"));
+dotenv.config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_CONNECT,() => console.log("connected to db"));
+
+app.use("/api/user", authRoute);
+
+app.use(express.json());
+
+app.listen(5000 , () => console.log("server started"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.post('/register', (req,res) => {
-    res.send('register');
+router.post('/register', async(req,res) => {
+    const user = new User({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+    });
+
+    try{
+        const savedUser = await User.save();
+        res.send(savedUser);
+    }catch(error){
+        res.status(400).send(err);
+    }
+
 })
 
 module.exports = router;
